dc3a9a3e0402e61b554df8ff0ef3e1181ec62978
Suleman Chaudhary <Chaudhary.98@buckeyemail.osu.edu>
2025-10-17 17:40:48 -0400

    Co-authored-by: akhaliq0 <akhaliq0@users.noreply.github.com> Co-authored-by: Sheraz Ahmed <SsjRaz@users.noreply.github.com> Co-authored-by: Talalahmedk7 <Talalahmedk7@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,17 +1,3 @@
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-import nba_api 
-
-from nba_api.stats.endpoints import playercareerstats
-from nba_api.stats.static import players
-
-
-careers = players.find_players_by_full_name("Nikola Jokic")
-career_id = careers[0]["id"]
-
-df = playercareerstats.PlayerCareerStats(player_id=career_id).get_data_frames()[0]
-
-
-print(df)
+import argparse
+from pathlib import Path
 
